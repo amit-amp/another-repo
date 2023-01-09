6b463f6190650c2953ab6ff9d0699a8319bb0872
Amit Barletz <amit@amplication.com>
2023-01-09 13:32:19 +0200

    update author service

--- server/src/author/author.service.ts
+++ server/src/author/author.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
 import { PrismaService } from "../prisma/prisma.service";
+import { Author } from "./base/Author";
 import { AuthorServiceBase } from "./base/author.service.base";
 
 @Injectable()
 export class AuthorService extends AuthorServiceBase {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma);
+  }
+
+  findMany<T extends Prisma.AuthorFindManyArgs>(args: Prisma.SelectSubset<T, Prisma.AuthorFindManyArgs>): Promise<Author[]> {
+    return this.prisma.author.findMany(args);
   }
 }
